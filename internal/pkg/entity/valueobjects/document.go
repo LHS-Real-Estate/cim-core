f73fd1de9110fd6ba47bbdb3458807b2f897b529
Hugo Yamauthi Silva <hugo.yamauthi@gmail.com>
2023-11-09 01:43:36 -0300

    feat: :fire: Remove unnecessary field and functions from document

--- internal/pkg/entity/valueobjects/document.go
+++ internal/pkg/entity/valueobjects/document.go
@@ -1,50 +1,6 @@
 package valueobjects
 
-import (
-	"errors"
-	"regexp"
-	"strings"
-
-	"github.com/LHS-Real-Estate/cim-core/internal/pkg/validator"
-)
-
 type Document struct {
-	Name      string `validate:"required"`
 	FilePath  string `validate:"required,filepath"`
-	Extension string `validate:"required,lowercase"`
-}
-
-var (
-	ErrInvalidFilePath = errors.New("the document filepath must have the file name")
-)
-
-const EXT_REGXP = "(\\.[^.]+)$"
-
-func NewDocument(name string, filePath string) (Document, error) {
-	reg := regexp.MustCompile(EXT_REGXP)
-	match := reg.FindStringSubmatch(name)
-	ext := ""
-
-	if match != nil {
-		ext = strings.ToLower(match[0])
-	}
-
-	document := Document{
-		Name:      name,
-		FilePath:  filePath,
-		Extension: strings.Replace(ext, ".", "", 1),
-	}
-
-	return document, validate(document)
-}
-
-func validate(d Document) error {
-	cv := validator.NewCustomValidate()
-	err := cv.Validate(d)
-
-	if !strings.HasSuffix(d.FilePath, d.Name) {
-		err = errors.Join(err, ErrInvalidFilePath)
-	}
-
-	return err
+	Extension string `validate:"required,lowercase,min=2"`
 }
